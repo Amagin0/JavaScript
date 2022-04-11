6e85be4f9994efa264bf89e0d411304d6887f6b7
Amagin0 <91787621+Amagin0@users.noreply.github.com>
2022-04-12 01:41:27 +0900

    [add]switch

---
+++ sc_1/switch.js
@@ -0,0 +1,29 @@
+// switch
+// if文のほうが見やすい
+
+const data = 1;
+
+switch (data) {
+  case 1:
+    console.log('1です')
+    break;
+  case 2:
+    console.log('2です')
+    break;
+  case 3:
+    console.log('3です')
+    break;
+  default:
+    console.log('1-3ではありません')
+    break;
+}
+
+if (data === 1) {
+  console.log('1です')
+}
+if (data === 2) {
+  console.log('2です')
+}
+if (data === 3) {
+  console.log('3です')
+}
