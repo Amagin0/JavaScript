a01b35118b1ec867e6136db9d0772dde5d796b2c
Amagin0 <91787621+Amagin0@users.noreply.github.com>
2022-04-12 02:09:59 +0900

    [add]function

---
+++ sc_1/function.js
@@ -0,0 +1,87 @@
+/* ユーザー定義関数 */
+
+/*
+function test(引数){
+  // 処理
+
+  // 戻り値
+  return 戻り値;
+}
+*/
+
+// 引数なし
+// 戻り値なし
+function test() {
+  console.log('テスト');
+}
+
+test();
+
+
+// 引数あり
+// 戻り値なし
+const comment = 'コメント'
+
+function getComment(string) {
+  console.log(string);
+}
+
+getComment(comment);
+
+// 引数なし
+// 戻り値あり
+function getNumberOfComment() {
+  return 5;
+}
+
+/* 表示されない */
+getNumberOfComment();
+
+/* 表示される */
+console.log(getNumberOfComment());
+
+const NumberOfComment = getNumberOfComment();
+console.log(NumberOfComment);
+
+// 引数　２つ
+// 戻り値あり
+function sumPrice(int1, int2) {
+  let int3 = int1 + int2;
+  return int3;
+}
+
+const total = sumPrice(3, 5);
+console.log(total);
+
+
+
+/* 組み込み関数 */
+const test_2 = 'テスト';
+const test_3 = 'です';
+console.log(test_2.length);
+
+console.log(test_2.concat(test_3));
+
+
+/* string.trim()...両端から空白などを取り除く */
+const greeting = '  Hello world!   ';
+console.log(greeting);
+// expected output: "  Hello world!   ";
+
+console.log(greeting.trim());
+// expected output: "Hello world!";
+
+
+/* string.substring()...〇文字目から〇文字だけ取り出す */
+const str = 'Mozilla';
+console.log(str.substring(1, 3));
+// expected output: "oz"
+// 3 は含まない
+
+console.log(str.substring(2));
+// expected output: "zilla"
+
+
+/* string.replace()...置き換え */
+const test_4 = 'テストです';
+console.log(test_4.replace('テスト', 'ファイト'));
